a3c2b86b430eda213f822f2dca815c3309e2d69b
Shane Gable <gableshane@gmail.com>
2019-11-11 15:50:31 -0800

    starts salmon cookie project

---
+++ app.js
@@ -0,0 +1,96 @@
+var hours = ['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ','7pm: '];
+
+var Seattle = {
+    name : 'Seattle',
+    minCustomers : 23,
+    maxCustomers : 65,
+    averageCustomers : 6.3,
+    sales : [],
+    getSales : function() {
+        var rand = Math.random();
+        var range = this.maxCustomers - this.minCustomers
+        var custPerHour = this.minCustomers + Math.floor(rand * range);
+        return custPerHour
+    }
+}
+var Tokyo = {
+    name : 'Tokyo',
+    minCustomers : 3,
+    maxCustomers : 24,
+    averageCustomers : 1.2,
+    sales : [],
+    getSales : function() {
+        var rand = Math.random();
+        var range = this.maxCustomers - this.minCustomers
+        var custPerHour = this.minCustomers + Math.floor(rand * range);
+        return custPerHour
+    }
+}
+var Dubai = {
+    name : 'Dubai',
+    minCustomers : 11,
+    maxCustomers : 38,
+    averageCustomers : 3.7,
+    sales : [],
+    getSales : function() {
+        var rand = Math.random();
+        var range = this.maxCustomers - this.minCustomers
+        var custPerHour = this.minCustomers + Math.floor(rand * range);
+        return custPerHour
+    }
+}
+var Paris = {
+    name : 'Paris',
+    minCustomers : 20,
+    maxCustomers : 38,
+    averageCustomers : 2.3,
+    sales : [],
+    getSales : function() {
+        var rand = Math.random();
+        var range = this.maxCustomers - this.minCustomers
+        var custPerHour = this.minCustomers + Math.floor(rand * range);
+        return custPerHour
+    }
+}
+var Lima = {
+    name : 'Lima',
+    minCustomers : 2,
+    maxCustomers : 16,
+    averageCustomers : 4.6,
+    sales : [],
+    getSales : function() {
+        var rand = Math.random();
+        var range = this.maxCustomers - this.minCustomers
+        var custPerHour = this.minCustomers + Math.floor(rand * range);
+        return custPerHour
+    }
+}
+function getSalesData(location) {
+    for (var i = 0; i < hours.length; i++) {
+        var numCookies = Math.ceil(location.getSales() * location.averageCustomers)
+        location.sales[i] = hours[i] + numCookies + ' cookies';
+    }
+}
+function displayData(location) {
+    var salesData = location.sales;
+    var getDiv = document.getElementById('div1');
+    var ul = document.createElement('ul');
+    getDiv.appendChild(ul);
+    ul.textContent = location.name;
+    for (var i = 0; i < salesData.length; i++) {
+        var li = document.createElement('li');
+        var listItem = ul.appendChild(li);
+        li.textContent = location.sales[i];
+    }
+}
+getSalesData(Seattle);
+getSalesData(Tokyo)
+getSalesData(Dubai)
+getSalesData(Paris)
+getSalesData(Lima)
+
+displayData(Seattle);
+displayData(Tokyo)
+displayData(Dubai)
+displayData(Paris)
+displayData(Lima)
